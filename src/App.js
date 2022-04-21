f6eee14b942a9650508aa4f50d0d16346c8a956b
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-04-21 08:32:20 +0100

    prepared the parent component and folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.component';
+import Main from './components/Main.component';
+import Experience from './components/Experience';
+import Footer from './components/Footer.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <div className="comoponents">
+       <Header/>
+       <Main/>
+       <Experience/>
+       <Footer/>
+     </div>
     </div>
   );
 }
