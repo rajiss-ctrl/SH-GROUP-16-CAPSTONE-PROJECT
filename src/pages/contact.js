b3337288cc3416bccdc03b9b699db1e9152a5f29
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2022-04-21 16:21:59 +0100

    react-router-dom installed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 function Contact(){
     return(
         <div>
+            <nav>
+                <button> <Link to ="/">Home</Link></button>
+                <button> <Link to ="/about">About</Link></button>
+                <button> <Link to ="/product">Product</Link></button>
+                <button> <Link to ="/contact">Contact</Link></button>
+            </nav>
             <h1> This is the Contact us page</h1>
         </div>
     )
