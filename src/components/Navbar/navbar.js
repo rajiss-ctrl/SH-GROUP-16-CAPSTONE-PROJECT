7d5b36a336e362160e810b5660d6f0812a87677e
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-04-26 15:36:23 +0100

    font awesome included and the navbar responsiveness completed

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,20 +1,49 @@
 import React from 'react';
+import {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 import { NavLink, Link } from 'react-router-dom';
 
+
+
+
+
+
+
 function Navbar() {
+    const[open, setOpen] = useState(false);
+    
+
+    const handleClick = ()=>{
+       setOpen(!open)
+       console.log("i am clicked") 
+    }
+   
     return (
         <div className='Navbar'>
             <nav className="Navbar-links">
                 <Link to='/' className="logo">
                     <img src='FoodieZonelogo.png' alt='FoodieZone' />
                 </Link>
-                <ul>
+                <ul  className={open  ? "expanded": "barMenu"} >
                     <li> <NavLink className={(navData) => navData.isActive ? "active" : ""} to="/">Home</NavLink></li>
                     <li> <NavLink className={(navData) => navData.isActive ? "active" : ""} to="/about">About</NavLink></li>
                     <li> <Link to="/product">Product</Link></li>
                     <li> <NavLink className={(navData) => navData.isActive ? "active" : ""} to="/contact">Contact</NavLink></li>
                 </ul>
+                <div  className='bar'>
+                    <FontAwesomeIcon icon={faTimes} className="faBars"
+                    style={{display: open ? "block" : "none"}}
+                    onClick={handleClick}
+                    ></FontAwesomeIcon>:
+            
+                    <FontAwesomeIcon icon={faBars} className="faBars"
+                         style={{display: !open ? "block" : "none"}}
+                     onClick={handleClick}
+                    ></FontAwesomeIcon>  
+                </div>
+                
             </nav>
         </div>
     );
